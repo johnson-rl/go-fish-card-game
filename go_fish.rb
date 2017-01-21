9731dbe7cc06ab8b82c6e628090ce62017ee2771
johnson-rl <johnson.rl@gmail.com>
2017-01-20 16:09:50 -0800

    made....some progress

--- go_fish.rb
+++ go_fish.rb
@@ -1,9 +1,36 @@
+
 class PlayingCard
+  attr_accessor :RANKS, :SUITS, :SORTED_CARDS
+  @RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+  @SUITS = ["C", "D", "H", "S"]
+  @SORTED_CARDS = [
+    "AC", "AD", "AH", "AS", "2C", "2D", "2H", "2S", "3C", "3D", "3H", "3S",
+    "4C", "4D", "4H", "4S", "5C", "5D", "5H", "5S", "6C", "6D", "6H", "6S",
+    "7C", "7D", "7H", "7S", "8C", "8D", "8H", "8S", "9C", "9D", "9H", "9S",
+    "10C", "10D", "10H", "10S", "JC", "JD", "JH", "JS", "QC", "QD", "QH", "QS",
+    "KC", "KD", "KH", "KS"
+  ]
+  attr_reader :rank, :suit, :face, :card
   # initialize
+  def initialize (arg)
+    @rank = arg[:rank]
+    @suit = arg[:suit]
+    @face = @rank+@suit
+  end
+  def to_s
+    @face
+  end
 end
 
 class CardDeck
-  # initialize
+  attr_reader :cards
+  def initialize(*bool)
+    @cards = []
+    52.times do
+      @cards.push(one = PlayingCard.new({rank: "a", suit: "d"}))
+    end
+    return @cards
+  end
 end
 
 class HandOfCards
